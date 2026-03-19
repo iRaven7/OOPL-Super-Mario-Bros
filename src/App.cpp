8f9c2d34862824f4c09d0e91da156293d6863a6a
iRaven7 <cory.lin10@gmail.com>
2026-03-20 00:10:38 +0800

    test update

--- src/App.cpp
+++ src/App.cpp
@@ -11,9 +11,9 @@ void App::Start() {
 }
 
 void App::Update() {
-    
+
     //TODO: do your things here and delete this line <3
-    
+
     /*
      * Do not touch the code below as they serve the purpose for
      * closing the window.
